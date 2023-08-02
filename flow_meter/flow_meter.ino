b03ce1eac055a5fc920d9d79f91166d7b7c44f53
LordHunter <huntertmgon@gmail.com>
2023-08-02 07:10:52 -0500

    modificaciones solo para demostracion

--- flow_meter/flow_meter.ino
+++ flow_meter/flow_meter.ino
@@ -1,24 +1,17 @@
-#include "ADS1X15.h"
 
 int FLOW_PIN = 2;
-double FLOW_RATE;
-volatile int COUNT;
-int FLOW_CONSTANTE = 4.00;  // 1 L / 250 pulsos segun especificacion de fabricante * 1000 mL | Este valor es solo proporcionado por el fabricante pero puede variar
+float FLOW_RATE;
+volatile float COUNT;
+float FLOW_CONSTANTE = 4.00;  // 1 L / 250 pulsos segun especificacion de fabricante * 1000 mL | Este valor es solo proporcionado por el fabricante pero puede variar
+float WATER_FLOW_MEASURE;
 
 void setup() {
   Serial.begin(9600);
   pinMode(FLOW_PIN, INPUT);
   attachInterrupt(0, FLOW, RISING);
-  Wire.begin();
-  Serial.println(__FILE__);
-  Serial.print("ADS1X15_LIB_VERSION: ");
-  Serial.println(ADS1X15_LIB_VERSION);
-
-  ADS.begin();
 }
 
 void loop() {
-  ADS.setGain(0);
   COUNT = 0;
   interrupts();
   delay(1000);
@@ -28,9 +21,9 @@ void loop() {
   WATER_FLOW_MEASURE = FLOW_RATE;
   FLOW_RATE = FLOW_RATE * 60;
   FLOW_RATE = FLOW_RATE / 1000;
-
-  Serial.println("FLOWRATE: " + FLOW_RATE + " l/min");
-  Serial.println("WATERFLOW_MEASURE: " + WATERFLOW_MEASURE " ml");
+  Serial.println(COUNT);
+  Serial.print("FLOWRATE: ");Serial.print(FLOW_RATE, 1);Serial.println(' l/m');
+  Serial.print("WATERFLOW: ");Serial.print(WATER_FLOW_MEASURE, 1);Serial.println(' ml/pulso');
 }
 
 void FLOW() {
--- sensor_presion_v3/sensor_presion_v3.ino
+++ sensor_presion_v3/sensor_presion_v3.ino
@@ -2,11 +2,11 @@
 #include "Arduino.h"
 #include "Wire.h"  //allows communication over i2c devices
 
-const int pressureZero = 6291.45;                //analog reading of pressure transducer at 0psi or 0MPa
-const int pressureMax = 58982.4;                //analog reading of pressure transducer at 1450.38psi or 10MPa
+const int pressureZero = 6291.45;              //analog reading of pressure transducer at 0psi or 0MPa
+const int pressureMax = 58982.4;               //analog reading of pressure transducer at 1450.38psi or 10MPa
 const int pressuretransducermaxPSI = 1450.38;  //psi value of transducer being used
 const int baudRate = 9600;                     //constant integer to set the baud rate for serial monitor
-const int sensorreadDelay = 1000;            //constant integer to set the sensor read delay in milliseconds
+const int sensorreadDelay = 1000;              //constant integer to set the sensor read delay in milliseconds
 
 float pressureValue = 0;  //variable to store the value coming from the pressure transducer
 
@@ -14,7 +14,6 @@ ADS1115 ADS(0x48);
 
 void setup()  //setup routine, runs once when system turned on or reset
 {
-
   Serial.begin(baudRate);  //initializes serial communication at set baud rate bits per second
   Wire.begin();
   Serial.println(__FILE__);
@@ -26,17 +25,16 @@ void setup()  //setup routine, runs once when system turned on or reset
 
 void loop()  //loop routine runs over and over again forever
 {
-  ADS.setGain(0);
-  int16_t val_0 = ADS.readADC(0);
-  float vol_0 = ADS.toVoltage(val_0);
+  int16_t val_0 = ADS.readADC(0);      // read analog value from A0 in ADS
+  float vol_0 = ADS.toVoltage(val_0);  // convert analog value into voltage value
   pressureValue = val_0;
-  pressureValue = ((pressureValue - pressureZero) * pressuretransducermaxPSI) / (pressureMax - pressureZero);  //conversion equation to convert analog reading to psi                                                                      // voltage factor                                                                 // voltage factor
-  // Serial.print("\tAnalog0: ");
-  // Serial.println(val_0);
-  // Serial.print("\tVoltage: ");
-  // Serial.println(vol_0);
-  //reads value from input pin and assigns to variable
-  Serial.print("psi: ");             //prints label to serial
-  Serial.println(pressureValue, 1);  //prints value from previous line to serial
+  pressureValue = ((pressureValue - pressureZero) * pressuretransducermaxPSI) / (pressureMax - pressureZero);  //conversion equation to convert analog reading to psi
+
+  Serial.print("\tAnalog0: ");
+  Serial.println(val_0);
+  Serial.print("\tVoltage: ");
+  Serial.println(vol_0, 5);  // print voltage with 5 decimals
+  Serial.print("psi: ");
+  Serial.println(pressureValue, 1);  //prints presasure value from decimal
   delay(sensorreadDelay);            //delay in milliseconds between read values
 }
